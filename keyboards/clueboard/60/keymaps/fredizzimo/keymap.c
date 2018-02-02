1b27f8d8d6b389db1be3a35d0222e06f090679f9
Fred Sundvik <fsundvik@gmail.com>
2018-02-03 00:22:01 +0200

    Fixed the fredizzimo Clueboard keymap

--- keyboards/clueboard/60/keymaps/fredizzimo/keymap.c
+++ keyboards/clueboard/60/keymaps/fredizzimo/keymap.c
@@ -4,9 +4,9 @@
 #define _______ KC_TRNS
 
 enum keyboard_layers {
-    _BL,
-    _FL,
-    _CL
+    BASE,
+    FUNC,
+    EXTRA,
 };
 
 enum custom_keycodes {
@@ -33,25 +33,28 @@ enum custom_keycodes {
   float song_zelda_puzzle[][2]  = SONG(ZELDA_PUZZLE);
 #endif
 
+#define MO_FUNC MO(FUNC)
+#define MO_EXTR MO(EXTRA)
+#define OS_MEH OSM(MOD_MEH)
+
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BL] = KEYMAP(
-      KC_MPLY, SV_1,    SV_2,    SV_3,    SV_4,    SV_5,    SV_6,    SV_7,    SV_8,   SV_9,    SV_0,    SV_PLUS, SV_ACUT, SV_BSPC, SV_DEL,\
-      SV_TAB,  SV_Q,    SV_W,    SV_E,    SV_R,    SV_T,    SV_Y,    SV_U,    SV_I,   SV_O,    SV_P,    SV_AA,   SV_UMLT, KC_NO,   \
-      SV_ESC,  SV_A,    SV_S,    SV_D,    SV_F,    SV_G,    SV_H,    SV_J,    SV_K,   SV_L,    SV_OE,   SV_AE,   SV_APOS, SV_ENT,  \
-      SV_LSFT, SV_LT,   SV_Z,    SV_X,    SV_C,    SV_V,    SV_B,    SV_N,    SV_M,   SV_COMM, SV_DOT,  SV_MINS, SV_RSFT, SV_BSLS, \
-      KC_LCTL, KC_LGUI, KC_LALT,                        KC_SPC,                       KC_RALT, KC_MENU, MO(FL),  SV_RCTL),
-    [_FL] = KEYMAP(
+    [BASE] = KEYMAP(
+      KC_MPLY, SV_1,    SV_2,    SV_3,    SV_4,    SV_5,    SV_6,    SV_7,    SV_8,   SV_9,    SV_0,    SV_PLUS, SV_ACUT, KC_BSPC, KC_DEL,\
+      KC_TAB,  SV_Q,    SV_W,    SV_E,    SV_R,    SV_T,    SV_Y,    SV_U,    SV_I,   SV_O,    SV_P,    SV_AA,   SV_UMLT, KC_NO,  \
+      KC_ESC,  SV_A,    SV_S,    SV_D,    SV_F,    SV_G,    SV_H,    SV_J,    SV_K,   SV_L,    SV_OE,   SV_AE,   SV_APOS, KC_ENT, \
+      KC_LSFT, SV_LT,   SV_Z,    SV_X,    SV_C,    SV_V,    SV_B,    SV_N,    SV_M,   SV_COMM, SV_DOT,  SV_MINS, KC_RSFT, OS_MEH, \
+      KC_LCTL, KC_LGUI, KC_LALT,                        KC_SPC,                       KC_RALT, KC_MENU, MO_FUNC, KC_RCTL),
+    [FUNC] = KEYMAP(
       KC_ESC,  KC_F1,  KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,  KC_F9,    KC_F10,  KC_F11,  KC_F12,  _______, _______,\
       _______, _______, KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______, _______, _______, _______, _______, _______, _______, _______, \
-      _______, MO(CL),  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______, _______, _______, _______, _______, _______, _______, \
+      _______, MO_EXTR, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______, _______, _______, _______, _______, _______, _______, \
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-      _______, _______, _______,                        _______,                       _______, _______, MO(_FL), _______),
-    [_CL] = KEYMAP(
-      BL_STEP, S_BSKTC, S_ODEJY, S_RCKBY, S_DOEDR, S_SCALE, S_ONEUP, S_COIN, S_SONIC, S_ZELDA,  _______, _______, _______, _______, _______,\
+      _______, _______, _______,                        _______,                       _______, _______, MO_FUNC, _______),
+    [EXTRA] = KEYMAP( BL_STEP, S_BSKTC, S_ODEJY, S_RCKBY, S_DOEDR, S_SCALE, S_ONEUP, S_COIN, S_SONIC, S_ZELDA,  _______, _______, _______, _______, _______,\
       _______, _______, _______, _______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-      _______, MO(CL),  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+      _______, MO_EXTR, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-      _______, _______, _______,                        _______,                       _______, _______, MO(_FL), _______)
+      _______, _______, _______,                        _______,                       _______, _______, MO_FUNC, _______)
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
