e48ce2f494ab79dc439b68dd8a7a3ae6a1bd6b13
Fred Sundvik <fsundvik@gmail.com>
2018-06-30 01:28:41 +0300

    Add polled_wait functions

--- tmk_core/common/wait.h
+++ tmk_core/common/wait.h
@@ -13,8 +13,21 @@ extern "C" {
 #   define wait_us(us)  _delay_us(us)
 #elif defined PROTOCOL_CHIBIOS
 #   include "ch.h"
+#   ifdef STM32_HSECLK
+#       define SYS_FREQ STM32_HSECLK
+#   elif defined(KINETIS_SYSCLK_FREQUENCY)
+#       define SYS_FREQ KINETIS_SYSCLK_FREQUENCY
+#   else
+#       error "The system frequency is unknown in this platform"
+#   endif
+
+#   define NS22RTC(freq, nsec) (rtcnt_t)((((freq) + 999999UL) / 1000000UL) * nsec / 1000)
+
 #   define wait_ms(ms) chThdSleepMilliseconds(ms)
 #   define wait_us(us) chThdSleepMicroseconds(us)
+#   define polled_wait_ns(ns) chSysPolledDelayX(NS22RTC(SYS_FREQ, ns))
+#   define polled_wait_us(us) chSysPolledDelayX(US2RTC(SYS_FREQ, us))
+#   define polled_wait_ms(ms) chSysPolledDelayX(MS2RTC(SYS_FREQ, ms))
 #elif defined(__arm__)
 #   include "wait_api.h"
 #else  // Unit tests
