34016088c5fe6128c2afaa41c21bc30090251921
Fred Sundvik <fsundvik@gmail.com>
2018-07-16 08:34:30 +0300

    Improvemets to unique_id

--- tmk_core/common/unique_id.c
+++ tmk_core/common/unique_id.c
@@ -1,10 +1,21 @@
-#include "uuid.h"
+#include "unique_id.h"
+#include <string.h>
+#ifdef PROTOCOL_CHIBIOS
+#include "ch.h"
+#include "hal.h"
+#endif
 
+// Some MCUs don't define UID_BASE even if it's available
+#ifndef UID_BASE
+#ifdef STM32F303xC
+#define UID_BASE ((uint32_t)0x1FFFF7ACU)
+#endif
+#endif
 
 #ifdef UID_BASE
 uint8_t* get_unique_id(void) {
   static unique_id_t id = {};
-  static initialized = false;
+  static bool initialized = false;
   if (!initialized) {
     memcpy(id, (void*)UID_BASE, 12);
     initialized = true;
@@ -12,3 +23,7 @@ uint8_t* get_unique_id(void) {
   return id;
 }
 #endif
+
+bool are_unique_ids_same(uint8_t* id1, uint8_t* id2) {
+  return memcmp(id1, id2, sizeof(unique_id_t)) == 0;
+}
--- tmk_core/common/unique_id.h
+++ tmk_core/common/unique_id.h
@@ -1,8 +1,9 @@
 #pragma once
 
 #include <stdint.h>
+#include <stdbool.h>
 
 typedef uint8_t unique_id_t[16];
 
-
 uint8_t* get_unique_id(void);
+bool are_unique_ids_same(uint8_t* id1, uint8_t* id2);
