3071b119b4ff5f64fc132d6d99358b171647c4d4
Fred Sundvik <fsundvik@gmail.com>
2021-06-10 13:39:32 +0300

    Reset state on certain keys

--- layouts/community/ergodox/fredizzimo/keymap.c
+++ layouts/community/ergodox/fredizzimo/keymap.c
@@ -312,6 +312,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         reset_state();
       }
       break;
+    case KC_BSPACE:
+    case KC_DEL:
+    case KC_ESC:
+    case KC_SPACE:
+    case KC_ENTER:
+      reset_state();
     }
     return true;
 }
